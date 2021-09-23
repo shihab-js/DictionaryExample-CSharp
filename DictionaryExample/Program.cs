1273f186dff4f6102a75e8d30599f76d59ee59af
Md. Shihab <shihabrayhan8@gmail.com>
2021-09-23 13:02:16 +0600

    Elements are added in dictionary....

--- DictionaryExample/Program.cs
+++ DictionaryExample/Program.cs
@@ -10,6 +10,13 @@ namespace DictionaryExample
             //create dictionary..
             Dictionary<int, string> names = new Dictionary<int, string>();
 
+            //add elements
+            names.Add(1, "Shihab");
+            names.Add(1, "Mishu");
+            names.Add(1, "Mithila");
+            names.Add(1, "Rayhan");
+
+
 
         }
     }
